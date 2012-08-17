d61c6cefa2cacb98df635d7b5d91ac66439654b2
sfoster <sam@sam-i-am.com>
2012-08-17 17:53:20 +0100

    Sketching out a Page class to hang page lifecyle and glue code off of

---
+++ js/lib/page.js
@@ -0,0 +1,20 @@
+define(['vendor/compose', 'vendor/knockout'], function(Compose, ko){
+
+  var Page = Compose(Compose, {
+    initialize: function(options){
+      
+      return this;
+    },
+    applyBindings: function(){
+      var selfNode = $(this.el)[0];
+      ko.applyBindings(this, selfNode);
+      return this;
+    },
+    setStore: function(store){
+      var self = this;
+      store = this.store = store;
+    
+      return this;
+    },
+  });
+});
