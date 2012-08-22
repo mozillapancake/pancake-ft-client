431c3000e28dec793d53694fb85b8d75eda3447f
sfoster <sam@sam-i-am.com>
2012-08-22 13:33:21 +0100

    The services store should be the single source of truth for all, keep config settings in there as well as the obvious query results

--- js/services/core.js
+++ js/services/core.js
@@ -3,8 +3,21 @@ define(['vendor/store/main'], function(store){
   // local proxy for data stored and shared across the application
   // could be seeded from localStorage
   // maybe use Cache to have a front and server-backed store
+  var storeData = [];
+  
+  // put our app settings in the store
+  if(config) {
+    var settings = config;
+    settings.id = 'settings';
+    storeData.push(settings);
+  }
+
+  // put the default user in the store
+  var defaultUser = { username: 'guest', id: 'user' };
+  storeData.push(defaultUser);
+  
   var dataStore = new store.Memory({
-    data: []
+    data: storeData
   });
 
   return dataStore;
