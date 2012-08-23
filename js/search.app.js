bc3a889700708339b55f34fb7b736dde0ed33bd7
sfoster <sam@sam-i-am.com>
2012-08-23 16:10:23 +0100

    Flesh out initial settings import into the store, and a user service on the store

--- js/search.app.js
+++ js/search.app.js
@@ -1,5 +1,6 @@
-define(['vendor/compose', 'lib/page', 'services/search'], function(Compose, Page, services){
+define(['dollar', 'compose', 'knockout', 'lib/knockout.composeWith', 'lib/page', 'services/search', 'services/user'], function($, Compose, ko, koComposeWith, Page, services){
   console.log("search.app loaded");
+  // console.assert(window.KO_M === ko);
   
   // various views, aggregated onto one page
   var app = Compose.create(Page, {
@@ -7,10 +8,21 @@ define(['vendor/compose', 'lib/page', 'services/search'], function(Compose, Page
   });
   // page has a lifecycle: initialize, assign store, applyBindings
   
+  window.services = services; 
+  
+  var user = services.user();
+  console.log("search.app, user is observable? ", ko.isObservable(user));
+  console.log("search.app, user has value ", user());
   var viewModel = {
     yourResults:    services.search.yourResults(),
     theirResults:   services.search.theirResults(),
-    webResults:     services.search.webResults()
+    webResults:     services.search.webResults(),
+    username: user.extend({ 
+      composeWith: [function(values){ 
+        console.log("username composeWith got: ", values);
+        return values.shift ? values.shift().username : values.username; 
+      }]
+    })
   };
   
   app.applyBindings(viewModel);
