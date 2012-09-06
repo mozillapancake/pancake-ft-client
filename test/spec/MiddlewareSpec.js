8809610d6e0b6891e22504310f564e78a78b9c40
sfoster <sam@sam-i-am.com>
2012-09-06 16:23:13 +0100

    Naive implementation of middleware adapter to get the first couple tests to pass

--- test/spec/MiddlewareSpec.js
+++ test/spec/MiddlewareSpec.js
@@ -1,15 +1,31 @@
-define(["dollar"], function($) {
+define(['dollar', 'promise'], function($, Promise) {
 
   // adapt a function to have a 'register' method, allowing handlers to be registered for calls to it
   var middleware = {
     adapt: function(fn, ctx) {
-      var middlewares = [];
+      var befores = [], 
+          afters: [];
+      var onError = function(err){ console.warn("Error: ", err); };
       var adapted = function(){
         var args = Array.prototype.slice.call(arguments);
 
         // get the subset of registered handlers that wish to participate based on the call args
-        var stack = middlewares.filter(function(mware){
-          return mware.accept.apply(mware, args);
+        var beforeStack = befores.slice();
+        var next = function(req, resp) {
+          var handler = beforeStack.shift();
+          // if handler.accept(req, resp)
+          handler(req, resp, function(){ })
+        }
+        beforeHandlers.forEach(function(mware){
+          if(mware.accept && !mware.accept(args)) {
+            return;
+          }
+          var next = function(){}
+          return mware.accept && mware.accept(args) ? mware(args, resp, next) : args;
+        });
+        
+        var stack = befores.filter(function(mware){
+          
         });
         
         // request phase
@@ -21,20 +37,22 @@ define(["dollar"], function($) {
         }, args);
 
         // call the original with the modified args to get a result
-        var result = fn.apply(ctx||null, args);
-
-        // response phase
-        result = stack.reduce(function(resp, mware){
-          if(mware.processResponse) {
-            resp = mware.processResponse.call(null, resp);
-          }
-          return resp;
-        }, result);
-        
-        return result;
+        var promise = Promise.when(fn.apply(ctx||null, args), function(result){
+          // response phase
+          result = stack.reduce(function(resp, mware){
+            if(mware.processResponse) {
+              resp = mware.processResponse.call(null, resp);
+            }
+            return resp;
+          }, result);
+          return result;
+        }, onError);
+        return promise;
       };
       adapted.original = fn;
-      adapted.register = function(mware) {
+      // register middleware function
+      // to be applied when something is true
+      adapted.registe = function() {
         middlewares.push(mware);
       };
       adapted.restore = function() {
@@ -47,6 +65,24 @@ define(["dollar"], function($) {
   
   describe("Middleware usage", function() {
 
+    var doubleArgs = function(a, b) {
+      return [ a*2, b*2 ];
+    };
+    var squareResult = function(res) {
+      return res*res;
+    };
+    var doubleAndThenSquare = {
+      accept: function(a,b){ var is = ('number'==typeof a && 'number'==typeof b); return is; },
+      processRequest: doubleArgs,
+      processResponse: squareResult
+    };
+    var reportResult = {
+      accept: Boolean,
+      processResponse: function(result) {
+        return "Result was: " + result;
+      }
+    };
+
     it("Loads and exposes the expected API", function(){
       expect(middleware).toBeTruthy();
       expect(typeof middleware.adapt).toBe('function');
@@ -54,39 +90,37 @@ define(["dollar"], function($) {
 
     it("Allows functions to be adapted to accept middleware", function(){
       var doSomething = function(a, b){ return a + b; };
-      var doubleArgs = function(a, b) {
-        return [ a*2, b*2 ];
-      };
-      var squareResult = function(res) {
-        return res*res;
-      };
-      var doubleAndThenSquare = {
-        accept: function(a,b){ var is = ('number'==typeof a && 'number'==typeof b); console.log("accept: ", a, b, is); return is; },
-        processRequest: doubleArgs,
-        processResponse: squareResult
-      };
+      var result; 
 
       // test function unadapted
       expect(doSomething(1,2)).toBe(3);
 
       // adapt it to allow registration of middleware
       doSomething = middleware.adapt(doSomething);
-      
       expect(typeof doSomething).toBe('function');
       expect(typeof doSomething.register).toBe('function');
 
       doSomething.register(doubleAndThenSquare);
-      
-      expect(doSomething(1,2)).toBe(36);
-      
-      // put it back how it was
-      doSomething = doSomething.restore();
-      expect(doSomething(2,3)).toBe(5);
-      
+
+      Promise.when(doSomething(1,2), function(res){
+        result = res;
+      });
+
+      waitsFor(function(){ return result; },1000);
+      runs(function(){
+        expect(result).toBe(36);
+
+        // put it back how it was
+        doSomething = doSomething.restore();
+        expect(doSomething(2,3)).toBe(5);
+      });
     });
 
     it("Allows registration of middleware to match certain requests", function(){
-      expect("TODO").toBe("Test implemented");
+      var doSomething = function(a, b){ return a + b; };
+      doSomething = middleware.adapt(doSomething);
+      doSomething.register(doubleAndThenSquare);
+      
     });
 
   });
