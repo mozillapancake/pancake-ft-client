05a1088d18b4fb536074061914b348d10c806205
sfoster <sam@sam-i-am.com>
2012-09-11 18:04:54 +0100

    Reworking the settings module to get/set selected values from localStorage.

--- js/services/settings.js
+++ js/services/settings.js
@@ -1,59 +1,111 @@
-define(['knockout', 'lang', 'store', 'lib/LsCacheStorage'], function(ko, lang, store, LocalStorage){
+define(['lang', 'EventEmitter', 'compose', 'knockout', 'store', 'lscache', 'lib/LsCacheStorage'], function(lang, EventEmitter, Compose, ko, store, lscache, LocalStorage){
   
   var undef;
-  var defaultSettings = {
+  var storedSettings = {
     username: "guest",
     session: ""
   };
+  var globalConfig = config || {};
+  var combinedSettings = lang.extend({}, storedSettings, globalConfig);
   
   var idPrefix = (config && config.localStorageIdPrefix) || 'pancake/';
-  idPrefix += 'settings/';
+  idPrefix += 'settings';
   
-  var settingsStore = store.Observable(new LocalStorage({
-    idPrefix: idPrefix
-  }));
+  console.log("combined settings: ", combinedSettings);
+  
+  var updateNotifier = new EventEmitter();
+  updateNotifier.addListener('change', function(key, value, oldValue){
+    // sync value with the storedSettings
+    settingsStore.put({
+      id: key, value: value
+    });
+    console.log("Observable setting value changed: ", key, value, oldValue);
+  });
+  
+  // internal store to normalize setting value access
+  //  some settings are localStorage-backed (see storedSettings), others not.
+  var settingsStore = Compose.create(Compose, {
+    // prototype
+    defaultExpiry: 15,
+    get: function(key) {
+      lscache.setBucket(idPrefix);
+      var value;
+      if(key in storedSettings){
+        value = lscache.get(key);
+      }
+      // fall back to defaults and/or config props
+      if(null === value || undef === value) {
+        value = combinedSettings[key];
+      }
+      return value;
+    },
+    reset: function(){
+      // reset to defaults, wiping out localStorage values
+      lscache.setBucket(idPrefix);
+      lscache.flush();
+      
+      // TODO: seems like some settings might be good forever (or a month, say)
+      // whereas others should expire quickly
+      var expireMinutes = 2; // Minutes
+      Object.keys(storedSettings).forEach(function(key){
+        lscache.set(key, storedSettings[key], expireMinutes);
+      });
+    },
+    put: function(object, options){
+      options = options || {};
+      var key = object.id || object.key; 
+      if(key in storedSettings){
+        lscache.setBucket(idPrefix);
+        lscache.set(key, object.value, options.expiry || this.defaultExpiry);
+      } else {
+        this[key] = object.value;
+      }
+      return key;
+    },
+    add: function(object, options){
+      options = options || options;
+      if(this.get(object.id)){
+        throw new Error("Object already exists");
+      }
+      return this.put(object, options);
+    },
+    registerSetting: function(key, value, options){
+      if('function' === typeof this[key]) {
+        throw "Cant register setting: " + key + " over existing function";
+      }
+      var initialValue = this.get(key);
+      var observable = this[key] = ko.observable( initialValue );
 
-  // settings is a dictionary of observable key=value
-  var settingsMap = {}; 
-
-  // overlay window.config properties
-  lang.each(lang.defaults(defaultSettings, config || {}), function(val, key){
-    updateEntry({ id: key, value: val });
+      var subscription = observable.subscribe(function(newValue){
+        updateNotifier.emitEvent('change', [key, newValue, value]);
+        // updateNotifier.emitEvent('change-'+key, [newValue, value]);
+      });
+      // stash subscription so we could clean up later
+      this._subscriptions[key] = subscription;
+    }
+  }, function(){
+    // init
+    // turn each property into an observable
+    this._subscriptions = {};
+    lang.each(combinedSettings, function(value, key){
+      this.registerSetting(key, value, {});
+    }, this);
   });
 
-  // get an observed resultset from the store representing all settings
-  var allSettings = settingsStore.query({ type: 'setting' });
-
-  function updateEntry(entry) {
-    // create/update observable settings values
-    if(settingsMap[entry.id]) {
-      settingsMap[entry.id](entry.value); 
-    } else {
-      settingsMap[entry.id] = new ko.observable(entry.value); 
+  function onStorage(evt){
+    var sourceUrl = evt.url || evt.uri;
+    if(sourceUrl !== location.href) {
+      console.log("Storage event from elsewhere: ", evt, sourceUrl);
     }
+    console.log("TODO: Handle storage event: ", evt, sourceUrl);
   }
-
-  // initial population of the settings from the query result
-  allSettings.forEach(updateEntry);
-
-  allSettings.observe(function(entry, fromIndex, toIndex){
-    if(toIndex === -1){
-      // setting removed
-      entry.value = undef;
-    }
-    updateEntry(details.object);
-  }, true);
-
-  // function onStorage(evt){
-  //   console.log("Storage event: ", evt);
-  // }
   // 
-  // if (window.addEventListener) {
-  //   window.addEventListener("storage", onStorage, false);
-  // } else {
-  //   window.attachEvent("onstorage", onStorage);
-  // }
-
-  return settingsMap;
+  if (window.addEventListener) {
+    window.addEventListener("storage", onStorage, false);
+  } else {
+    window.attachEvent("onstorage", onStorage);
+  }
+  console.log("Created settings module: ", settingsStore);
+  return settingsStore;
 
 });
