10e5e0fc82a9a54e727ab5c09ba40835bef1c1a5
sfoster <sam@sam-i-am.com>
2012-09-11 11:20:38 +0100

    Refactor data loader plugin to use the 'dollar' module for its xhr

--- js/plugin/data.js
+++ js/plugin/data.js
@@ -1,35 +1,23 @@
 /** @license
  * RequireJS plugin for loading JSON files *at runtime only*
- * - depends on Text plugin
  * Author: Sam Foster
- * Version: 0.1.0 (2012/03/13)
+ * Version: 0.2.0 (2012/09/11)
  * Released under the MIT license
  */
-define(['text'], function(text){
-
-  function get(url, callback){
-    var xhr = text.createXhr();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = function (evt) {
-        //Do not explicitly handle errors, those should be
-        //visible via console output in the browser.
-        if (xhr.readyState === 4) {
-            callback(xhr.responseText);
-        }
-    };
-    xhr.send(null);
-  }
-
-    //API
-    return {
-        load : function(name, req, onLoad, config) {
-          if(config.isBuild){
-            onLoad(null); //we want to load the resource at runtime, not inline during build
-          }else{
-            get(req.toUrl(name), function(data){
-              onLoad( JSON.parse(data) );
-            });
-          }
-        }
-    };
+define(['dollar'], function($){
+  // 'dollar' is expected to provide an .ajax method which works like jquery's $.ajax
+  return {
+    // Just define 'load' from the Loader plugin API
+    load : function(name, req, onLoad, config) {
+      if(config.isBuild){
+        onLoad(null); //we want to load the resource at runtime, not inline during build
+      }else{
+        $.ajax({ 
+          url: req.toUrl(name),
+          dataType: 'json',
+          success: onLoad
+        });
+      }
+    }
+  };
 });
