a9401cf226e057cd4b4878e95ef1f1a73896d047
sfoster <sam@sam-i-am.com>
2012-09-13 16:06:52 +0100

    Stub out url-opening APIs

--- js/pancake/main.js
+++ js/pancake/main.js
@@ -1,7 +1,18 @@
 define(function(){
   Pancake = {};
+
   Pancake.openApplicationView = function(url){
-    window.open(url, 'viewer');
+    console.log("Open %s in application view", url);
+    window.open(url, 'appviewer');
+  };
+
+  Pancake.openPublicView = function(url){
+    console.log("Open %s in public viewer", url);
+    window.open(url, 'publicviewer');
+  };
+  
+  Pancake.open = function(url){
+    location.assign(url);
   };
   
   return Pancake;
